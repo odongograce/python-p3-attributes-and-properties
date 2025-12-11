76f1a4729abde7abbc96a2dc317d3a5434d9f1fe
Grace Odongo <graceakinyi024@gmail.com>
2025-12-11 11:07:04 +0300

    new commit

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,16 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name, breed):
+        self.name=name
+        self.breed =breed
+    def set_name(self, name):
+        if (type(name) in (str)) and (1 <= name <= 25):
+            print(f"Setting name to { name }.")
+            self._name = name
+
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    # def set_breed(self, breed):
+        
+   
